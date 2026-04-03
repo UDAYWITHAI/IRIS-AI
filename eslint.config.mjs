b08d61b447120e9e95539e910bc7bd15b0060fc9
Harsh <pandeygautam039@gmail.com>
2026-04-04 00:48:08 +0530

    update eslint.config.mjs and WorkFlowEditor.tsx

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -25,7 +25,15 @@ export default defineConfig(
     },
     rules: {
       ...eslintPluginReactHooks.configs.recommended.rules,
-      ...eslintPluginReactRefresh.configs.vite.rules
+      ...eslintPluginReactRefresh.configs.vite.rules,
+      '@typescript-eslint/no-unused-vars': [
+        'warn',
+        {
+          argsIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+          caughtErrorsIgnorePattern: '^_'
+        }
+      ]
     }
   },
   eslintConfigPrettier
